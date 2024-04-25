7b78fc0df79ed52697fca44bcab70057ab5c02ad
Kumar Harshvardhan <sharshu1999hraj@gmail.com>
2024-04-25 09:53:26 +0530

    Changing blue mode to dark mode

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@ export default function Navbar(props) {
         </form> */}
         <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
             <input className="form-check-input mx-1" type="checkbox" role="switch" onClick={props.toggleModeBlue} id="flexSwitchCheckDefault" />
-            <label className="form-check-label" htmlFor="flexSwitchCheckDefault" >Enable {props.mode==='light'?'blue':'light'} Mode</label>
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault" >Enable {props.mode==='light'?'dark':'light'} Mode</label>
         </div>
         {/* <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
             <input className="form-check-input mx-1" type="checkbox" role="switch" onClick={props.toggleMode} id="flexSwitchCheckDefault" />
